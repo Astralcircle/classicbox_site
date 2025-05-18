d6299470374dd59f93ee3ec4da8c5da1e53d12e4
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-05-18 19:30:55 +0300

    Add config/store screenshots only on server

---
+++ loadscreen/config.js
@@ -0,0 +1,7 @@
+//MAX_SCREENSHOTS = MAX_SCREENSHOTS + 1 if you want to add new screenshoot
+//Drop screenshoot in loadscreen/screenshoots and name them image{MAX_SCREENSHOTS}.jpg
+const MAX_SCREENSHOTS =  0;
+const SUBTITLES = [
+    "Уже заварили чаек?", "Добро пожаловать!",
+    "Всегда рады видеть вас снова!", "Понравился сервер? Вступай в наш дискорд!"
+];
