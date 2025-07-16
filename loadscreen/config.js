086e89a049d1908201ee9a787b03ea527f931a08
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-07-16 14:50:55 +0300

    Convert to tabs Like them more

--- loadscreen/config.js
+++ loadscreen/config.js
@@ -2,6 +2,6 @@
 //Drop screenshoot in loadscreen/screenshoots and name them image{MAX_SCREENSHOTS}.jpg
 const MAX_SCREENSHOTS =  0;
 const SUBTITLES = [
-    "Уже заварили чаек?", "Добро пожаловать!",
-    "Всегда рады видеть вас снова!", "Понравился сервер? Вступай в наш дискорд!"
+	"Уже заварили чаек?", "Добро пожаловать!",
+	"Всегда рады видеть вас снова!", "Понравился сервер? Вступай в наш дискорд!"
 ];
