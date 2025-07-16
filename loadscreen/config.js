4cea91a725af01447b99d24c6aa1b69b94ff3e9c
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-07-16 14:53:38 +0300

    Style fix

--- loadscreen/config.js
+++ loadscreen/config.js
@@ -1,6 +1,6 @@
 //MAX_SCREENSHOTS = MAX_SCREENSHOTS + 1 if you want to add new screenshoot
 //Drop screenshoot in loadscreen/screenshoots and name them image{MAX_SCREENSHOTS}.jpg
-const MAX_SCREENSHOTS =  0;
+const MAX_SCREENSHOTS = 0;
 const SUBTITLES = [
 	"Уже заварили чаек?", "Добро пожаловать!",
 	"Всегда рады видеть вас снова!", "Понравился сервер? Вступай в наш дискорд!"
